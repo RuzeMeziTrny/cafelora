4566bdbb6ee46bf71df42fdead2eddfcd73cb2fd
Ivana Ruzickova <shopping@mezitrny.cz>
2020-11-01 11:35:04 +0100

    hide nav on click

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const closeNav = () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+};
+
+const hamburgerMenu = document.querySelector('#nav-btn');
+hamburgerMenu.addEventListener('click', closeNav);
+
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', closeNav);
+});
