3324976aa60b3013d14562e3dcc152cab9f3fc36
Ivana Ruzickova <shopping@mezitrny.cz>
2020-11-01 12:06:41 +0100

    order button functionality

--- src/index.js
+++ src/index.js
@@ -14,3 +14,19 @@ const navLinks = document.querySelectorAll('nav a');
 navLinks.forEach((navLink) => {
   navLink.addEventListener('click', closeNav);
 });
+
+let ordered = false;
+
+const orderBtn = document.querySelector('.order-btn');
+const drinkCup = document.querySelector('.drink__cup');
+orderBtn.addEventListener('click', () => {
+  if (ordered) {
+    drinkCup.classList.remove('drink__cup--selected');
+    orderBtn.textContent = 'Objednat';
+    ordered = false;
+  } else {
+    drinkCup.classList.add('drink__cup--selected');
+    orderBtn.textContent = 'Zrušit';
+    ordered = true;
+  }
+});
